0596c69111f57f60a24ecc316fb09aa34969b20d
sevenpia <sevenpia@naver.com>
2022-12-09 17:55:53 +0900

    jst 221209

---
+++ js/1.js
@@ -0,0 +1,6 @@
+var today = new Date();
+var year = today.getFullYear()
+var month = today.getMonth();
+var date = today.getDate();
+var day = today.getDay();
+document.write(`${year}년 ${month}월 ${date}일 ${day}요일`);
